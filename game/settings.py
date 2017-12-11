b4e65a8531c86d186206919ae704e30a56746f3d
Shamika La Shawn <Sg3147@utsnyc.edu>
2017-12-10 19:21:22 -0500

    made changes to settings.py file debug changed to false

--- game/settings.py
+++ game/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ycg(3d4&ou26+jd&0!zepvb-)%ag=v8n@q7&)g@*j$6e-+-1b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '.herokuapp.com/', 'boiling-gorge-18646.herokuapp.com']
 
